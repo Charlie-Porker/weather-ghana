1ce0ea1150a37ece57911e09c5a86304ddf68635
Charlie-Porker <ronaldhardy666.rh@gmail.com>
2026-04-27 16:32:14 +0000

    added weather route

---
+++ app/routers/weather.py
@@ -0,0 +1,18 @@
+from fastapi import APIRouter
+import requests
+
+router =APIRouter(prefix="/weather")
+
+@router.get("/{city}")
+
+def get_weather(city):
+    url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
+    response = requests.get(url)
+    data = response.json
+    lats = data["results"][0]["latitude"]
+    lons = data["results"][0]["longitude"]
+    wea_url = f"https://api.open-meteo.com/v1/forecast?latitude={lats}&longitude={lons}&current_weather=true"
+    wea_response = requests.get(wea_url)
+    wea_data = wea_response.json()
+    temp = wea_data["current_weather"]["temperature"]
+    return {"city": city, "temperature": temp}
