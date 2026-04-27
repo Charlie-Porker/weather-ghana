4ffa5c66e132a554ae7855351395b76d30ab7c83
Charlie-Porker <ronaldhardy666.rh@gmail.com>
2026-04-27 21:23:48 +0000

    added error handling for weather route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
+from app.routers import weather
+
 
 app = FastAPI()
+
+app.include_router(weather.router)
 
 @app.get("/")
 def root():
--- app/routers/weather.py
+++ app/routers/weather.py
@@ -1,14 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
 import requests
 
-router =APIRouter(prefix="/weather")
+router = APIRouter(prefix="/weather")
 
 @router.get("/{city}")
 
 def get_weather(city):
     url = f"https://geocoding-api.open-meteo.com/v1/search?name={city}&count=1"
     response = requests.get(url)
-    data = response.json
+    data = response.json()
+    if not data.get("results"):
+        raise HTTPException(status_code=404, detail="city not found")
     lats = data["results"][0]["latitude"]
     lons = data["results"][0]["longitude"]
     wea_url = f"https://api.open-meteo.com/v1/forecast?latitude={lats}&longitude={lons}&current_weather=true"
